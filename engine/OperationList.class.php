934c2a73d12fbf82b69868f1c2778beabe67eba8
rostoves <rostoves@users.noreply.github.com>
2019-02-10 23:23:32 +0300

    split oplist to new template

--- engine/OperationList.class.php
+++ engine/OperationList.class.php
@@ -4,7 +4,8 @@ class OperationList
 {
     public static function getOperationsList()
     {
-        $oplist =  Database::getInstance()->getColumns('*','[operations_list]');
+        $conditions = ['[status]' => ['=','\'OK\'']];
+        $oplist =  Database::getInstance()->getColumnsWhereMultiple('TOP (1000) *','[operations_list]', $conditions,' ORDER BY [operation_date] DESC');
         Log::getLog()->info("Returned operations list: " . count($oplist) . " rows");
         Log::getLog()->trace("Full operations list data: ". print_r($oplist,1));
         return $oplist;
